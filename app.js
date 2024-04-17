7a4de50a1b7487d2e8d60979a742ded882fabc3b
BlackNubia <johnajibolajaiyeoba@gmail.com>
2024-04-16 23:06:12 -0700

    fixed the purchase btn

--- app.js
+++ app.js
@@ -1,195 +1,285 @@
-const header = document.querySelector("header");
+// const header = document.querySelector("header");
 
-    window.addEventListener ('scroll', function() {
-        header.classList.toggle('sticky', this.window.scrollY >  0)
-    });
+// window.addEventListener('scroll', function() {
+//     header.classList.toggle('sticky', this.window.scrollY > 0);
+// });
+
+// let menu = document.querySelector('#menu-icon');
+// let navmenu = document.querySelector('.navmenu');
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navmenu.classList.toggle('open');
+// };
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const eventList = document.getElementById('eventList');
+//     const events = [
+        // {
+        //     name : 'Fire and Sand',
+        //     date : '12-23-2024',
+        //     image : 'ass/c1kpcehqivz3scqmkzuo.webp',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // },
+        // {
+        //     name : 'Yhemo Lee',
+        //     date : '12-23-2024',
+        //     image : 'ass/jr3ilqkz1ufildgmtukx.webp',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // },
+        // {
+        //     name : 'Abarie.com',
+        //     date : '12-23-2024',
+        //     image : 'ass/2272dab8-fea5-4547-b1b6-407f0fb32f41.jpeg',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // },
+        // {
+        //     name : 'Soycity',
+        //     date : '12-23-2024',
+        //     image : 'ass/mlgfp86j5jmghhkxexkz.jpg.webp',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // },
+        // {
+        //     name : 'Destalker Laughter Crusade',
+        //     date : '12-23-2024',
+        //     image : 'ass/IMG_2233-768x888.jpeg',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // },
+        // {
+        //     name : 'High Voltage Doings',
+        //     date : '12-23-2024',
+        //     image : 'ass/IMG_0633-768x960.jpeg',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 22,
+        //     }
+        // },{
+        //     name : 'SDLiC',
+        //     date : '12-23-2024',
+        //     image : 'ass/D9EEBD20-C08A-49B0-A40F-A565DFDA380E.jpeg',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //     }
+        // },
+        // {
+        //     name : 'AY Live',
+        //     date : '12-23-2024',
+        //     image : 'ass/d83e177c-be90-49e5-8e93-a65630bbcb99-768x481.jpeg',
+        //     price: {
+        //         regular: 2000,
+        //         vip: 15000,
+        //         premium: 30000
+        //     }
+        // }
+
+//     ];
+
+
+//     function displayEvents() {
+//         eventList.innerHTML = '';
+//         events.forEach(row => {
+//             const eventElement = document.createElement('div');
+//             eventElement.classList.add('row');
+//             eventElement.innerHTML = `
+//                 <img src='${row.image}' alt='${events.name}' >
+//                 <div class="product-text">
+//                     <h5>Sale</h5>
+//                 </div>
+//                 <div class="price">
+//                     <h4>${row.name}</h4> 
+//                     <p>Date: ${row.date}</p>
+//                     <h4>Price: Regular ${row.price.regular}, VIP ${row.price.vip}, Premium ${row.price.premium}</h4>
+//                 </div>
+//                 <button class="orderBtn" onclick="buyTickets('${row.name}', '${row.date}')">Buy Tickets</button>  
+
+           
+
+           
+//                 `;
+//             eventList.appendChild(eventElement);
+//         });
+//     }
+
+//     displayEvents();
+// });
+
+
+// // Show ticket checkout form for the selected event
+// function buyTickets(name, date) {
+//     const eventNameField = document.getElementById('name');
+//     const eventDateField = document.getElementById('date');
+
+//     eventNameField.value = name;
+//     eventDateField.value = date;
+
+//     document.getElementById('ticketCheckoutForm').style.display = 'block';
+// }
+
+ 
+
+// function buyTickets() {
+//     document.getElementById('ticketCheckoutForm').style.display = 'block';
+    
+// }
+
+// Navigation
+
+ const header = document.querySelector("header");
+
+window.addEventListener('scroll', function() {
+    header.classList.toggle('sticky', this.window.scrollY > 0);
+});
 
 let menu = document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
-    menu.onclick = () => {
-        menu.classList.toggle('bx-x');
-        navmenu.classList.toggle('open');
-    }
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+};
+
 
 document.addEventListener('DOMContentLoaded', function() {
     const eventList = document.getElementById('eventList');
-
-    // events/concerts
-
-    const events = [
+    const events = [ 
         {
             name : 'Fire and Sand',
             date : '12-23-2024',
             image : 'ass/c1kpcehqivz3scqmkzuo.webp',
-            price : '7000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         },
         {
             name : 'Yhemo Lee',
             date : '12-23-2024',
             image : 'ass/jr3ilqkz1ufildgmtukx.webp',
-            price : '5000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         },
         {
             name : 'Abarie.com',
             date : '12-23-2024',
             image : 'ass/2272dab8-fea5-4547-b1b6-407f0fb32f41.jpeg',
-            price : '5000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         },
         {
             name : 'Soycity',
             date : '12-23-2024',
             image : 'ass/mlgfp86j5jmghhkxexkz.jpg.webp',
-            price : '5000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         },
         {
             name : 'Destalker Laughter Crusade',
             date : '12-23-2024',
             image : 'ass/IMG_2233-768x888.jpeg',
-            price : '5000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         },
         {
             name : 'High Voltage Doings',
             date : '12-23-2024',
             image : 'ass/IMG_0633-768x960.jpeg',
-            price : '10000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 22,
+            }
         },{
             name : 'SDLiC',
             date : '12-23-2024',
             image : 'ass/D9EEBD20-C08A-49B0-A40F-A565DFDA380E.jpeg',
-            price : '5000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+            }
         },
         {
             name : 'AY Live',
             date : '12-23-2024',
             image : 'ass/d83e177c-be90-49e5-8e93-a65630bbcb99-768x481.jpeg',
-            price : '3000.00'
+            price: {
+                regular: 2000,
+                vip: 15000,
+                premium: 30000
+            }
         }
-
-    ];
-    // {
-    //   const  result = [
-    //       "repeat(1,2,3,4,5,6,7)",
-    //       {
-    //         "status": "disabled",
-    //         "name": {
-    //           "first": "Jamison",
-    //           "middle": "Austin",
-    //           "last": "Rath"
-    //         },
-    //         "username": "Jamison-Rath",
-    //         "password": "GHJ0f1h38KN0UNh",
-    //         "emails": [
-    //           "Vivianne_Johnson78@gmail.com",
-    //           "Erling_Wintheiser@gmail.com"
-    //         ],
-    //         "phoneNumber": "1-834-265-3679 x4573",
-    //         "location": {
-    //           "street": "674 Hermann Path",
-    //           "city": "New Camryn",
-    //           "state": "Oklahoma",
-    //           "country": "Latvia",
-    //           "zip": "74257-5940",
-    //           "coordinates": {
-    //             "latitude": 65.7431,
-    //             "longitude": 5.4732
-    //           }
-    //         },
-    //         "website": "https://glamorous-face.info",
-    //         "domain": "kaleidoscopic-marshland.com",
-    //         "job": {
-    //           "title": "Forward Branding Associate",
-    //           "descriptor": "Forward",
-    //           "area": "Web",
-    //           "type": "Engineer",
-    //           "company": "Balistreri Group"
-    //         },
-    //         "creditCard": {
-    //           "number": "58937468123383696357",
-    //           "cvv": "011",
-    //           "issuer": "diners_club"
-    //         },
-    //         "uuid": "14afcfc1-b57f-48ba-a9b1-6f2db659894e",
-    //         "objectId": "660b12da4f6fd8d4546ae713"
-    //       }
-    //     ]
-    //   }
-
-    // functon to display events
-
+     ];
 
     function displayEvents() {
         eventList.innerHTML = '';
-        events.forEach(row => {
+        events.forEach(event => {
             const eventElement = document.createElement('div');
             eventElement.classList.add('row');
             eventElement.innerHTML = `
-            <img src='${row.image}' alt='${events.name}' >
-            <div class="product-text">
-                        <h5>Sale</h5>
-                    </div>
-                    <div class="price">
-            <h4 > ${row.name}</h4> 
-            <p>Date: ${row.date} </p>
-            <h4>Price: #${row.price}</h4>
-            </div>
-             <button class="orderBtn" onclick=""  id="buyBtn"> Buy Tickets </button>  
-           
-
+                <img src="${event.image}" alt="${event.name}">
+                <div class="product-text">
+                    <h5>Sale</h5>
+                </div>
+                <div class="price">
+                    <h4>${event.name}</h4> 
+                    <p>Date: ${event.date}</p>
+                    <h4>Price: 
+                        Regular ${event.price.regular}, 
+                        VIP ${event.price.vip},
+                        Premium ${event.price.premium}
+                    </h4>
+                </div>
+                <button class="orderBtn" onclick="buyTickets('${event.name}', '${event.date}')">Buy Tickets</button>
             `;
-
-           // selectTickets(' ${row.name}', ${row.price})
             eventList.appendChild(eventElement);
         });
-    } 
+    }
+
     displayEvents();
-
-    // function to handle selecting tickets
-
-    window.selectTickets = function(eventName, price) {
-        const tickets = prompt(`How many Tickets do  you need? ${eventName}`);
-        if (tickets !== null && !isNaN(tickets) && tickets > 0){
-            const total = price * tickets;
-            alert(`You selected ${tickets} tickets for ${eventName}. Total: #${total}`);
-        } else {
-            alert(`Invald Request! Please enter a valid number  ` )
-        }
-    };
 });
 
-/* 
-    window.selectTickets = function() {
-    // Get event name and price from form fields
-    const eventName = document.getElementById('eventName').value;
-    const price = document.getElementById('price').value;
-    const tickets = document.getElementById('tickets').value;
+function buyTickets(name, date, ) {
+    const eventNameField = document.getElementById('name');
+    const eventDateField = document.getElementById('date');
+   
+
+    eventNameField.value = name;
+    eventDateField.value = date;
     
-    // Validate the number of tickets
-    if (tickets !== "" && !isNaN(tickets) && tickets > 0){
-        const total = price * tickets;
-        alert(`You selected ${tickets} tickets for ${eventName}. Total: ${total}`);
-    } else {
-        alert(`Invalid Request! Please enter a valid number of tickets.`);
-    }
-};
-*/
-
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("buyBtn");
-var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function() {
-        modal.style.display = "block";
-    } 
-    span.onclick = function(){
-        modal.style.display ="none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  
+    document.getElementById('ticketCheckoutForm').style.display = 'block';
+    window.location.href = `checkout.html?eventName=${encodeURIComponent(name)}&eventDate=${encodeURIComponent(date)}`;
+    
 }
-const 
-
-function buyBtn(){
-    mod
-} 
